9930e26ecfc59eb0121f54091948434450a78ba5
Hítallo William <hitallo91@gmail.com>
2024-04-11 17:18:16 -0300

    feat: improve list boletos with pages

--- app/domain/usecases/list_boletos.py
+++ app/domain/usecases/list_boletos.py
@@ -1,26 +1,29 @@
 from typing import List
 
+from app.domain.contracts.boleto_repository import BoletoRepositoryContract
 from app.domain.contracts.usecase import BaseData, Usecase
 
 
-class ListBoletoInput(BaseData):
-    limit: int
-    offset: int
+class ListBoletoRequest(BaseData):
+    limit: int | None = 100
+    offset: int | None = 0
+    boleto_id: int | None = None
 
 
 class ListBoletoResponse(BaseData):
+    total_items: int
     boletos: List
 
 
 class ListBoletoUsecase(Usecase):
 
-    def execute(self, data: ListBoletoInput) -> ListBoletoResponse:
+    def __init__(self, boleto_repository: BoletoRepositoryContract) -> None:
+        self.boleto_repository = boleto_repository
 
+    def execute(self, data: ListBoletoRequest) -> ListBoletoResponse:
+
+        boletos = self.boleto_repository.get_all(limit=data.limit, offset=data.offset)
         return ListBoletoResponse(
-            boletos=[
-                {"id": 1},
-                {"id": 2},
-                {"id": 3},
-                {"id": 4},
-            ]
+            total_items=len(boletos),
+            boletos=boletos,
         )
