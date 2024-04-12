4428f20dca14c2f93c8a6352a88794add41d96dd
Hítallo William <hitallo91@gmail.com>
2024-04-12 19:30:44 -0300

    fix: adjust load env

--- app/main/queue.py
+++ app/main/queue.py
@@ -1,7 +1,11 @@
 from time import sleep
 
+from dotenv import load_dotenv
+
 from app.infra.logging.logging import Logging
 from app.infra.queue.sqs import Sqs
+
+load_dotenv()
 
 if __name__ == "__main__":
     print("starting ...")
