47898c5b7227324b286a307d558bd529217f88d7
Hítallo William <hitallo91@gmail.com>
2024-04-13 19:24:56 -0300

    feat: add list boleto by id

--- app/domain/usecases/list_boletos.py
+++ app/domain/usecases/list_boletos.py
@@ -2,13 +2,14 @@ from typing import List
 
 from app.domain.contracts.repository_factory import RepositoryFactoryContract
 from app.domain.contracts.usecase import Usecase
+from app.domain.entities.boleto import Boleto
 from app.domain.entities.entity import Entity
 
 
 class ListBoletoRequest(Entity):
     limit: int | None = 100
     offset: int | None = 0
-    boleto_id: int | None = None
+    boleto_id: str | None = None
 
 
 class ListBoletoResponse(Entity):
@@ -16,12 +17,15 @@ class ListBoletoResponse(Entity):
     boletos: List
 
 
-class ListBoletoUsecase(Usecase):
+class ListBoletoUsecase(Usecase[ListBoletoRequest, ListBoletoResponse | Boleto]):
 
     def __init__(self, repository_factory: RepositoryFactoryContract) -> None:
         self.boleto_repository = repository_factory.get_boleto_repository()
 
-    def execute(self, data: ListBoletoRequest) -> ListBoletoResponse:
+    def execute(self, data: ListBoletoRequest) -> ListBoletoResponse | Boleto:
+
+        if data.boleto_id:
+            return self.boleto_repository.get_by_id(data.boleto_id)
 
         page = self.boleto_repository.get_all(
             limit=data.limit,
