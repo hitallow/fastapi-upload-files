88fc520d0e002c855d2225810fe9c3ac56068c94
Hítallo William <hitallo91@gmail.com>
2024-04-13 16:30:27 -0300

    feat: register on sqs handler factory

--- app/presentation/factories/sqs_handler_factory.py
+++ app/presentation/factories/sqs_handler_factory.py
@@ -3,8 +3,13 @@ from typing import Dict
 from app.domain.contracts.handler import Handler
 from app.presentation.factories.handle_import_csv_factory import \
     handle_import_csv
+from app.presentation.factories.handle_notify_boletos_factory import \
+    handle_notify_boletos_factory
 
 
 def sqs_handler_factory(event_name: str, event_body: Dict) -> Handler | None:
     if event_name == "importBoletosFromCSV":
         return handle_import_csv(event_body)
+
+    if event_name == 'notifyBoletos':
+        return handle_notify_boletos_factory(event_body)
