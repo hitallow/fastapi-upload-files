b865683ec6cd5395306fc0c06808c5e2d469162b
Hítallo William <hitallo91@gmail.com>
2024-04-13 19:44:30 -0300

    feat: handling publish error

--- app/domain/usecases/init_notify_boletos.py
+++ app/domain/usecases/init_notify_boletos.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from app.domain.contracts.third_party_factory import ThirdPartyFactoryContract
 from app.domain.contracts.usecase import Usecase
 from app.domain.entities.entity import Entity
+from app.domain.exceptions.publish_queue_task_exception import \
+    PublishQueueTaskException
 from app.domain.usecases.handle_notify_boletos import HandleNotifyBoletosEvent
 
 
@@ -21,21 +23,28 @@ class InitNotifyBoletos(Usecase[InitNotifyBoletosRequest, InitNotifyBoletosRespo
         self.queue = third_party_factory.get_queue()
 
     def execute(self, data: InitNotifyBoletosRequest) -> InitNotifyBoletosResponse:
-        today = datetime.today()
+        try:
+            today = datetime.today()
 
-        self.logging.info(
-            f"iniciando leitura dos boletos do dia  {today.strftime('%d/%m/%Y')}"
-        )
+            self.logging.info(
+                f"iniciando leitura dos boletos do dia  {today.strftime('%d/%m/%Y')}"
+            )
 
-        today = today.replace(
-            hour=0,
-            minute=0,
-            second=0,
-            microsecond=0,
-        )
+            today = today.replace(
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0,
+            )
 
-        self.queue.publish(HandleNotifyBoletosEvent(from_date=int(today.timestamp())))
+            self.queue.publish(
+                HandleNotifyBoletosEvent(from_date=int(today.timestamp()))
+            )
 
-        self.logging.info("enfileriado")
+            self.logging.info("enfileriado")
+
+        except PublishQueueTaskException as publish_error:
+            # notificar alguem via email talvez ....
+            self.logging.error("erro ao publicar na fila")
 
         return InitNotifyBoletosResponse()
