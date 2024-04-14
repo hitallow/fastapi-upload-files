59301227bbe245ce7c4f675928d5acc3f2a39aab
Hítallo William <hitallo91@gmail.com>
2024-04-14 16:24:11 -0300

    test: add new methods

--- tests/helpers/fake_database.py
+++ tests/helpers/fake_database.py
@@ -4,9 +4,21 @@ class FakeDB:
 
     def commit(self):
         return self
-    
+
     def fetchone(self):
+        return self
+    
+    def fetchall(self):
         return self
 
     def execute(self, *params):
         return self
+    
+    def executemany(self, *params):
+        return self
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *params):
+        pass
