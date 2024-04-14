e77a688251c0e7c306dbf5fdfffbd02f60985f3c
Hítallo William <hitallo91@gmail.com>
2024-04-14 15:39:15 -0300

    fix: adjust

--- tests/infra/database/repositories/file_repository_test.py
+++ tests/infra/database/repositories/file_repository_test.py
@@ -8,23 +8,20 @@ from tests.helpers.generate_entities import make_file
 
 
 def test_should_insert_file():
-    with patch.object(Connection, "get_database") as db:
-        mocked_db = FakeDB()
-
-        db.return_value = mocked_db
-
+    mocked_db = FakeDB()
+    with patch.object(Connection, "get_database", return_value=mocked_db):
         repository = FileRepository()
 
-        file = make_file()
+    file = make_file()
 
-        mocked_db.commit = MagicMock()
-        mocked_db.cursor = MagicMock(return_value=mocked_db)
-        mocked_db.execute = MagicMock()
+    mocked_db.commit = MagicMock()
+    mocked_db.cursor = MagicMock(return_value=mocked_db)
+    mocked_db.execute = MagicMock()
 
-        saved = repository.insert_one(file)
+    saved = repository.insert_one(file)
 
-        assert isinstance(saved, File) == True
+    assert isinstance(saved, File) == True
 
-        mocked_db.commit.assert_called_once()
-        mocked_db.cursor.assert_called_once()
-        mocked_db.execute.assert_called_once()
+    mocked_db.commit.assert_called_once()
+    mocked_db.cursor.assert_called_once()
+    mocked_db.execute.assert_called_once()
